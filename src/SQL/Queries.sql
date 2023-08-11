ec5a90d40678adb254266ea2e8e6ac7a92f7896b
Kevin Day <kevin.ch.day@gmail.com>
2023-08-11 17:06:00 -0500

    Update 8/11/2023

--- src/SQL/Queries.sql
+++ src/SQL/Queries.sql
@@ -1,7 +1,5 @@
 -- Query records by sample families
-SELECT *
-FROM malware_samples
-WHERE family = 'Anubis';
+SELECT * FROM malware_samples WHERE family = 'Xenomorph';
 
 -- family and # of samples
 select family, count(*)
@@ -13,3 +11,12 @@ select *
 from malware_samples
 where Kaspersky_Label like '%Anubis%'
 order by id;
+
+-- Dataset Label
+SELECT id, Kaspersky_Label, Microsoft_Label
+FROM malware_samples WHERE family = 'BlackRock'
+
+-- MobSF Security Scorecard
+SELECT id, security_score, grade, trackers_detections, high_risks, medium_risk
+FROM mobfs_analysis
+WHERE id in (75, 76, 77)
