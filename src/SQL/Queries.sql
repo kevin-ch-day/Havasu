b35f48f4b74e2bac7b6a5eb9e1e08570a8de7998
Kevin Day <kevin.ch.day@gmail.com>
2023-08-17 17:24:25 -0500

    Update

--- src/SQL/Queries.sql
+++ src/SQL/Queries.sql
@@ -1,5 +1,7 @@
 -- Query records by sample families
-SELECT * FROM malware_samples WHERE family = 'Xenomorph';
+SELECT *
+FROM malware_samples
+WHERE family = 'Xenomorph';
 
 -- family and # of samples
 select family, count(*)
@@ -7,16 +9,61 @@ from malware_samples
 where family is not null
 GROUP by Family
 
-select *
-from malware_samples
-where Kaspersky_Label like '%Anubis%'
-order by id;
-
 -- Dataset Label
-SELECT id, Kaspersky_Label, Microsoft_Label
-FROM malware_samples WHERE family = 'BlackRock'
+SELECT x.id,
+    x.Kaspersky_Label Kaspersky,
+    x.Microsoft_Label Microsoft
+FROM x.malware_samples
+WHERE x.family = 'BlackRock'
 
--- MobSF Security Scorecard
-SELECT id, security_score, grade, trackers_detections, high_risks, medium_risk
-FROM mobfs_analysis
-WHERE id in (75, 76, 77)
+-- MobSF Security Scorecard by family
+SELECT y.id,
+	y.security_score score,
+    y.grade,
+    y.trackers_detections tracker,
+    y.high_risks,
+    y.medium_risk
+FROM malware_samples x
+	JOIN mobfs_analysis y
+    	ON y.id = x.id
+WHERE x.family = 'brata';
+
+-- static analysis chart
+select x.id,
+	x.size,
+	y.activities,
+    y.services,
+    y.receivers,
+    y.providers
+from malware_samples x
+	join mobfs_analysis y
+		on y.id = x.id
+where x.fami;y = 'brata'
+order by x.id;
+
+-- Dangerous Permissions
+SELECT x.permission_id,
+	x.permission_name
+FROM android_permissions x
+where x.permission_status = 'Dangerous'
+order by x.permission_name asc;
+
+-- non-dangerous permissions
+SELECT x.permission_id,
+	x.permission_name
+FROM android_permissions x
+where not x.permission_status = 'Dangerous'
+order by x.permission_name asc;
+
+-- normal permissions
+SELECT x.permission_id,
+	x.permission_name
+FROM android_permissions x
+where x.permission_status = 'Normal'
+order by x.permission_name asc;
+
+SELECT x.permission_id,
+	x.permission_name
+FROM android_permissions x
+where x.permission_status = 'Not for use by third-party apps.'
+order by x.permission_name asc;
