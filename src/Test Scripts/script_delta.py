d1c4f40b0bac7109f99387a1040fed5f8128f101
Kevin Day <kevin.ch.day@gmail.com>
2023-08-29 23:55:37 -0500

    Update 8/29/2023

--- src/Test Scripts/script_delta.py
+++ src/Test Scripts/script_delta.py
@@ -10,10 +10,49 @@ conn = mysql.connector.connect(
 )
 cursor = conn.cursor()
 
-sql = "SELECT * FROM malware_samples"
-sql = sql + " WHERE id in (4, 11, 20, 21, 54, 55, 122, 125)"
-sql = sql + " order by id"
+ANUBIS = "(55, 80, 81, 83, 103, 104, 105)"
+permissions = list()
+
+sql = "select * "
+sql = sql + "from detected_standard_permissions "
+sql = sql + "where id in " + ANUBIS
+sql = sql + "order by id"
+
+sql_query = pd.read_sql_query(sql, conn)
+df_alpha = pd.DataFrame(sql_query)
+df_beta = pd.DataFrame()
+
+df_beta['ID'] = df_alpha['ID']
+df_alpha = df_alpha.drop(columns=['ID'])
+
+for column in df_alpha:
+  for cell in df_alpha[column]:
+    if cell is not None:
+      df_beta[column] = df_alpha[column]
+      permissions.append(column)
+      break
+    # if
+  # for
+# for
+
+sql = "select * from android_permissions"
+sql = sql + " where Name in ("
+
+index = 0
+for p in permissions:
+  if index is (len(permissions)-1):
+    sql = sql + "'" + p + "'"
+  else:
+    sql = sql + "'" + p + "'" + ", "
+  # for
+  index = index + 1
+# for
+
+sql = sql + ") order by Protection_level, Name"
+
+#print("\n# permissions: " +str(len(permissions)))
+#print("\n" + sql + "\n") # DEBUGGING
 
 sql_query = pd.read_sql_query(sql, conn)
 df = pd.DataFrame(sql_query)
-df.to_excel('output.xlsx')
+df.to_excel('Results.xlsx')
